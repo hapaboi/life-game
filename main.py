d4e9fc3da3736ca59937f0623f4ae9c71f166d0b
Sean Carlyle <808jalapeno@gmail.com>
2022-05-06 00:03:21 -0700

    making dead board state

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 def dead_board(width, height):
+    empty = []
     for i in range(height):
+        empty.append([])
+        for j in range(width):
+            empty[i].append(0)
+    return empty
 
 
 def main():
     width = 5
     height = 5
-    dead_board(5,5)
+    dead = dead_board(width, height)
+    print(dead)
+
+
 if __name__ == '__main__':
     main()
 
